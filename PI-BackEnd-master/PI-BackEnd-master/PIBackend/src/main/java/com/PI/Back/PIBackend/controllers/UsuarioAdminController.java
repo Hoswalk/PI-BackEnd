9e3d6a089486f9fe412564e15fd09a425dc8e9cf
Mateo <quotterimateo@gmail.com>
2024-08-28 18:47:56 -0300

    adding crossorigin to controllers

--- PI-BackEnd-master/PI-BackEnd-master/PIBackend/src/main/java/com/PI/Back/PIBackend/controllers/UsuarioAdminController.java
+++ PI-BackEnd-master/PI-BackEnd-master/PIBackend/src/main/java/com/PI/Back/PIBackend/controllers/UsuarioAdminController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
