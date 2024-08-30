76bd8f186cfdd50c04f06a51cbbe027cfb0d4c64
Mateo <quotterimateo@gmail.com>
2024-08-30 14:23:38 -0300

    adding some things -with errors

--- PI-BackEnd-master/PI-BackEnd-master/PIBackend/src/main/java/com/PI/Back/PIBackend/config/SegurityConfig.java
+++ PI-BackEnd-master/PI-BackEnd-master/PIBackend/src/main/java/com/PI/Back/PIBackend/config/SegurityConfig.java
@@ -32,6 +32,7 @@ public class SegurityConfig {
                 .authorizeHttpRequests(authRequest ->
                         authRequest
                                 .requestMatchers("/auth/**").permitAll()
+                                .requestMatchers("/api/usuarios").permitAll()
                                 .anyRequest().authenticated()
 
                 )
